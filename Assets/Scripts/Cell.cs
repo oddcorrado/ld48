3092e2fab15e1eeeb7676b853b01f4f09259f943
corrado <corradolongoni@gmail.com>
2021-04-24 15:57:39 +0200

    Cell V0

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    [SerializeField] private Sprite[] backgroundSprites;
+    [SerializeField] private SpriteRenderer backgroundSprite;
+    public enum BackType { Earth, Water, Poison, Rock }
+    public enum RootDirection { Up, Down, Left, Right }
+
+
+    private BackType back;
+    public BackType Back
+    {
+        get { return back; }
+        set { back = value; backgroundSprite.sprite = backgroundSprites[(int)value]; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Back = BackType.Earth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
